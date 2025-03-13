cc93108d8d58fb574ea018ac61c9d279895eed8a
mrx7355608 <mrx7355608@gmail.com>
2025-03-14 03:12:53 +0500

    integrated ipinfo.io api for fetching user's region based on it's ip

---
+++ app/api/get-region/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  // Get IP
+  let ip: string | undefined = "";
+  if (process.env.NODE_ENV === "production") {
+    const forwarded = req.headers.get("x-forwarded-for");
+    ip = forwarded ? forwarded.split(",")[0] : undefined;
+    if (ip?.startsWith("::ffff:")) {
+      ip = ip.substring(7);
+    }
+  } else {
+    ip = "103.21.244.0";
+  }
+
+  // Get Region
+  const token = process.env.IP_INFO_TOKEN;
+  const url = `https://ipinfo.io/${ip}?token=${token}`;
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+
+  return NextResponse.json({ region: result.region });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
+"use client";
 import { Chat } from "@/components/chat";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const DEFAULT_REGION = "Washington";
+  const [region, setRegion] = useState("");
+
+  // Fetch user region from his ip
+  useEffect(() => {
+    const regionExist = localStorage.getItem("region");
+    if (regionExist) {
+      console.log("region already exists");
+      setRegion(regionExist);
+      return;
+    }
+
+    fetch("/api/get-region")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setRegion(data.region);
+        console.log(data);
+        localStorage.setItem("region", data.region);
+      })
+      .catch(console.error)
+      .finally(() => setRegion(DEFAULT_REGION));
+  }, []);
+
+  if (!region) {
+    return <p className="text-xl align-center">Loading...</p>;
+  }
+
   return (
     <main className="flex min-h-screen flex-col">
-      <Chat />
+      <Chat region={region} />
     </main>
   );
 }
